0358144845c003b406d4dacade19a447d5fb2822
Om Thapa <omthapa1904@gmail.com>
2025-09-21 22:55:47 +0530

    Add SignInOAuthButton component and integrate it into Topbar

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,35 @@
+import { SignedOut, UserButton } from "@clerk/clerk-react";
+import { LayoutDashboardIcon } from "lucide-react"
+import { Link } from "react-router-dom";
+import SignInOAuthButton from "./SignInOAuthButton";
+
+const Topbar = () => {
+    const isAdmin = false
+
+  return (
+    <div
+        className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10"
+    >
+        <div className="flex gap-2 items-center">
+            Spotify
+        </div>
+        <div className="flex items-center
+        gap-4">
+            {isAdmin && (
+                <Link to={"/admin"}>
+                    <LayoutDashboardIcon className="size-4 mr-2"/>
+                    Admin DashBoard
+                </Link>
+            )}
+        </div>
+
+        <SignedOut>
+            <SignInOAuthButton />
+        </SignedOut>
+
+        <UserButton />
+    </div>
+  )
+}
+
+export default Topbar
